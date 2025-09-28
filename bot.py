7d060b58aca4bd074c78ecc196ff88f7a44b4bb9
filter00 <alienorigin36940@gmail.com>
2025-09-28 20:14:29 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,103 @@
+import os
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from flask import Flask, redirect
+from threading import Thread
+from motor.motor_asyncio import AsyncIOMotorClient
+import asyncio
+
+API_ID = os.environ.get("API_ID","")
+API_HASH = os.environ.get("API_HASH","")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","")
+DATABASE_URL = os.environ.get("DATABASE_URL","")
+BOT_USERNAME = os.environ.get("BOT_USERNAME","") # Without @
+
+# Database
+client = AsyncIOMotorClient(DATABASE_URL)
+db = client['databas']
+groups = db['group_id']
+
+bot = Client(
+    "deletebot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=300,
+    sleep_threshold=10
+)
+
+@bot.on_message(filters.command("thewarriorsreal") & filters.private)
+async def start(_, message):
+    button = [[
+        InlineKeyboardButton("🎈 Aᴅᴅ ʏᴏᴜʀ Gʀᴏᴜᴘ 🎈", url=f""),
+        ]]
+    await message.reply_text(
+        f"I ᴀᴍ Aᴜᴛᴏ Dᴇʟᴇᴛᴇ Bᴏᴛ, I ᴄᴀɴ ᴅᴇʟᴇᴛᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘs ᴍᴇssᴀɢᴇs ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀғᴛᴇʀ ᴀ ᴄᴇʀᴛᴀɪɴ ᴘᴇʀɪᴏᴅ ᴏғ ᴛɪᴍᴇ.",
+        reply_markup=InlineKeyboardMarkup(button),
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
+
+@bot.on_message(filters.command("set"))
+async def set_delete_time(_, message):
+
+    if message.chat.type == enums.ChatType.PRIVATE:
+        await message.reply("Tʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ɪɴ ɢʀᴏᴜᴘs....😒")
+        return
+
+    args = message.text.split()
+    if len(args) < 2 or not args.isdigit():
+        await message.reply_text(
+            "Dᴇʟᴇᴛᴇ ᴛɪᴍᴇ ᴍᴜsᴛ ʙᴇ ᴀɴ ɴᴜᴍʙᴇʀ...\n\n"
+            "Exᴀᴍᴘʟᴇ: /sᴇᴛ 10\n"
+            "Exᴀᴍᴘʟᴇ: /sᴇᴛ 20\n"
+            "Oɴʟʏ Sᴇᴄᴏᴜɴᴅ 🙌"
+        )
+        return
+
+    delete_time = int(args)
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    admin_ids = []
+    async for member in bot.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+        admin_ids.append(member.user.id)
+
+    if user_id not in admin_ids:
+        await message.reply("Oɴʟʏ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs ᴄᴀɴ ᴅᴏ ᴛʜɪs....😘")
+        return
+
+    await groups.update_one(
+        {"group_id": chat_id},
+        {"$set": {"delete_time": delete_time}},
+        upsert=True
+    )
+    await message.reply_text(f"Sᴜᴄᴄᴇssғᴜʟʟʏ Sᴇᴛ {delete_time} Sᴇᴄᴏᴜɴᴅ....✅")
+
+@bot.on_message(filters.group & filters.text)
+async def delete_message(_, message):
+    chat_id = message.chat.id
+    group = await groups.find_one({"group_id": chat_id})
+    if not group:
+        return
+    delete_time = int(group["delete_time"])
+    try:
+        # Wait deletion time and delete message, regardless sender admin hai ya nahi
+        await asyncio.sleep(delete_time)
+        await message.delete()
+    except Exception as e:
+        print(f"An error occurred: {e} /n Group ID: {chat_id}")
+
+# Flask setup for keepalive web server
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(f"https://telegram.me/{BOT_USERNAME}", code=302)
+
+def run():
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
+
+if __name__ == "__main__":
+    t = Thread(target=run)
+    t.start()
+    bot.run()
